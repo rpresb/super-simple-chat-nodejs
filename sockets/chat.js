913d73e195569a266fe3c373d13216150a2c1a97
Rodrigo De Presbiteris <rpresb@hotmail.com>
2017-05-31 08:51:38 -0300

    New user connected and broadcast received

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,11 +1,20 @@
 var Chat = function (io) {
     'use strict';
 
+    var chat = {
+        users: []
+    };
+
     io.on('connection', function (socket) {
         socket.broadcast.emit('user connected');
 
         socket.on('new-user', function (userName) {
-            ChatServer.users.push(userName);
+            chat.users.push(userName);
+
+            io.sockets.emit('broadcast-new-user', {
+                payload: `${userName} is online`,
+                source: userName
+            });
         });
 
         socket.on('message', function (from, msg) {
@@ -20,9 +29,7 @@ var Chat = function (io) {
         });
     });
 
-    return ({
-        users: []
-    });
+    return (chat);
 };
 
 module.exports = Chat;
--- test/chat.js
+++ test/chat.js
@@ -1,7 +1,7 @@
 var should = require('should');
 var io = require('socket.io-client');
 
-var socketURL = 'http://0.0.0.0:5000';
+var socketURL = 'http://localhost:5000';
 
 var options = {
   transports: ['websocket'],
@@ -9,6 +9,32 @@ var options = {
 };
 
 describe("Chat Server", function () {
-  it('Should broadcast new user to all users');
+  it('Should broadcast new user to all users', function (done) {
+    var client1 = io.connect(socketURL, options);
+
+    client1.on('connect', function (data) {
+      client1.emit('new-user', 'User1');
+
+      var client2 = io.connect(socketURL, options);
+      client2.on('connect', function (data) {
+        client2.emit('new-user', 'User2');
+        client2.disconnect();
+      });
+
+      var numUsers = 0;
+      client1.on('broadcast-new-user', function (msg) {
+        numUsers++;
+
+        if (numUsers === 2) {
+          msg.source.should.equal('User2');
+          msg.payload.should.equal('User2 is online');
+
+          client1.disconnect();
+          done();
+        }
+      });
+    });
+  });
+
   it('Should broadcast messages');
 });
